0de36df9dc3079321a79e917a8ab10f70744d03d
Pablo Leonarduzzi <pablo.leonarduzzi@despegar.com>
2020-10-26 21:18:04 -0300

    Fix empty tasks

--- BancoAPI/src/main/java/com/banco/api/task/SalaryPaymentTask.java
+++ BancoAPI/src/main/java/com/banco/api/task/SalaryPaymentTask.java
@@ -4,6 +4,8 @@ import com.banco.api.model.scheduledTransaction.ScheduledTransactionStatus;
 import com.banco.api.model.scheduledTransaction.salary.SalaryPayment;
 import com.banco.api.repository.scheduledTransaction.SalaryPaymentRepository;
 import com.banco.api.service.SalaryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +17,8 @@ import static java.util.Optional.ofNullable;
 
 @Component
 public class SalaryPaymentTask implements MyTask {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SalaryPaymentTask.class);
 
     @Autowired
     private SalaryService salaryService;
@@ -30,8 +34,11 @@ public class SalaryPaymentTask implements MyTask {
 
     @Override
     public void execute() {
+        LOGGER.info("Executing Salary Payment Task");
         List<SalaryPayment> salaryPayments = salaryPaymentRepository
                 .findAllByScheduledDateBeforeAndStatus(new Date(), ScheduledTransactionStatus.PENDING.getValue());
+
+        LOGGER.info("Pending salary payments scheduled for today: {}", salaryPayments != null ? salaryPayments.size() : 0);
 
         ofNullable(salaryPayments).ifPresent(payments ->
                 payments.forEach(payment -> salaryService.paySalary(payment)));
--- BancoAPI/src/main/java/com/banco/api/task/ScheduledTransactionTaskExecutor.java
+++ BancoAPI/src/main/java/com/banco/api/task/ScheduledTransactionTaskExecutor.java
@@ -1,6 +1,8 @@
 package com.banco.api.task;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +13,19 @@ import static com.google.common.collect.Lists.newArrayList;
 @Component
 public class ScheduledTransactionTaskExecutor {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledTransactionTaskExecutor.class);
+
     private List<MyTask> tasks;
 
     //Everyday at 01:00:00
     @Scheduled(cron = "0 0 1 * * ?")
     public void executeTasks() {
-        tasks.forEach(MyTask::execute);
+        LOGGER.info("Executing scheduled transaction tasks process started");
+        if (CollectionUtils.isNotEmpty(tasks)) {
+            tasks.forEach(MyTask::execute);
+        } else {
+            LOGGER.info("No tasks to execute");
+        }
     }
 
     public void addTask(MyTask task) {
